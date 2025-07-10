d334dcfc072120f359c4f04a0e3e8fc24fae3a82
natsuokawai <kawai.natsuo@gmail.com>
2025-07-11 00:04:01 +0900

    problem 1

--- bin/problem_0001.ml
+++ bin/problem_0001.ml
@@ -1,1 +1,8 @@
-print_endline "hello"
+open Core
+
+let multiples_of_3_or_5 n =
+    List.range 1 n
+     |> List.filter ~f:(fun n -> n mod 3 = 0 || n mod 5 = 0)
+     |> List.fold ~init:0 ~f:(+)
+
+let () = printf "%d\n" (multiples_of_3_or_5 1000)

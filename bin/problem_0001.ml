d287e32a78ac103eb14916a9f77d7e5fae133bc7
natsuokawai <kawai.natsuo@gmail.com>
2025-07-12 17:29:33 +0900

    Setup OCamlFormat

--- bin/problem_0001.ml
+++ bin/problem_0001.ml
@@ -1,8 +1,8 @@
 open Core
 
 let multiples_of_3_or_5 n =
-    List.range 1 n
-     |> List.filter ~f:(fun n -> n mod 3 = 0 || n mod 5 = 0)
-     |> List.fold ~init:0 ~f:(+)
+  List.range 1 n
+  |> List.filter ~f:(fun n -> n mod 3 = 0 || n mod 5 = 0)
+  |> List.fold ~init:0 ~f:( + )
 
 let () = printf "%d\n" (multiples_of_3_or_5 1000)
--- bin/problem_0002.ml
+++ bin/problem_0002.ml
@@ -1,16 +1,12 @@
 open Core
 
 let even_fibonacci_numbers max_num =
-  let fib_seq = Seq.unfold (fun (a, b) ->
-    if a >= max_num then
-      None
-    else
-      Some (a, (b, a + b))
-  )   (1  ,  2)
+  let fib_seq =
+    Seq.unfold
+      (fun (a, b) -> if a >= max_num then None else Some (a, (b, a + b)))
+      (1, 2)
   in
-  
-  fib_seq
-  |> Seq.filter (fun n -> n mod 2 = 0)
-  |> Seq.fold_left (+) 0
+
+  fib_seq |> Seq.filter (fun n -> n mod 2 = 0) |> Seq.fold_left ( + ) 0
 
 let () = printf "%d\n" (even_fibonacci_numbers 4_000_000)

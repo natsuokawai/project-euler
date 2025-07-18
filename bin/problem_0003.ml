2b3df0c51a6f97cff16b4bd370e54a4dd7a6f0f7
natsuokawai <kawai.natsuo@gmail.com>
2025-07-19 08:28:02 +0900

    promlem 3

--- bin/problem_0003.ml
+++ bin/problem_0003.ml
@@ -7,12 +7,12 @@ let is_prime n : bool =
   let rec is_dividable d =
     d <= isqrt n && (rem n 2L = 0L || rem n d = 0L || is_dividable (d + 1L))
   in
-
   if n <= 1L then false else if n = 2L then true else not (is_dividable 2L)
 
-let rec largest_prime_factor n x =
-  if is_prime x && rem n x = 0L then x else largest_prime_factor n (x - 1L)
+let rec largest_prime_factor n d =
+  if not (is_prime d) then largest_prime_factor n (d + 1L)
+  else if n = d then n
+  else if rem n d = 0L then largest_prime_factor (n / d) d
+  else largest_prime_factor n (d + 1L)
 
-let () =
-  let target = 600851475143L in
-  print_endline (Int64.to_string (largest_prime_factor target (target / 2L)))
+let () = print_endline (Int64.to_string (largest_prime_factor 600851475143L 2L))

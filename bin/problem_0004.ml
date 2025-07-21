4b5e480a0a65b1eadf40e9ff310d015f55fff5ec
natsuokawai <kawai.natsuo@gmail.com>
2025-07-21 10:00:13 +0900

    revise problem 4 in imperative style

--- bin/problem_0004.ml
+++ bin/problem_0004.ml
@@ -5,20 +5,17 @@ let is_palidromic_num n =
   String.equal s (String.rev s)
 
 let largest_palindrome_product () =
-  let rec search_i i =
-    if i = 0 then None
-    else if is_palidromic_num i then
-      let rec search_j j =
-        if j < 100 then None
-        else if i mod j = 0 && i / j >= 100 && i / j <= 999 then Some i
-        else search_j (j - 1)
-      in
-      match search_j 999 with Some x -> Some x | None -> search_i (i - 1)
-    else search_i (i - 1)
-  in
-  search_i (999 * 999)
+  let max_palindrome = ref 0 in
 
-let () =
-  match largest_palindrome_product () with
-  | Some x -> Printf.printf "%d\n" x
-  | None -> Printf.printf "Not found."
+  for a = 999 downto 100 do
+    for b = a downto 100 do
+      let prod = a * b in
+      if prod > !max_palindrome && is_palidromic_num prod then
+        max_palindrome := prod
+      else ()
+    done
+  done;
+
+  !max_palindrome
+
+let () = Printf.printf "%d\n" (largest_palindrome_product ())

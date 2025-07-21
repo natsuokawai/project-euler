79605109eb173c66c25c081c48c2ecbb8ad35e15
natsuokawai <kawai.natsuo@gmail.com>
2025-07-21 11:26:37 +0900

    fix

--- bin/problem_0003.ml
+++ bin/problem_0003.ml
@@ -4,10 +4,10 @@ open Int64
 let isqrt n = Int64.to_float n |> Float.sqrt |> Float.to_int64
 
 let is_prime n : bool =
-  let rec is_dividable d =
-    d <= isqrt n && (rem n 2L = 0L || rem n d = 0L || is_dividable (d + 1L))
+  let rec is_divisible d =
+    d <= isqrt n && (rem n 2L = 0L || rem n d = 0L || is_divisible (d + 1L))
   in
-  if n <= 1L then false else if n = 2L then true else not (is_dividable 2L)
+  if n <= 1L then false else if n = 2L then true else not (is_divisible 2L)
 
 let rec largest_prime_factor n d =
   if not (is_prime d) then largest_prime_factor n (d + 1L)
